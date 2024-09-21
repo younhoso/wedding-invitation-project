1d1a6c1cb4876ec479b65ea2b1c32ca8841b0100
younhoso <thdbsgh3443@kakao.com>
2024-09-21 18:28:03 +0900

    Feat: 폰트 및 에러 컴포넌트 작업

--- src/libs/fonts.ts
+++ src/libs/fonts.ts
@@ -7,7 +7,15 @@ export const pretendard = localFont({
       weight: '900',
     },
     {
+      path: '../../public/fonts/Pretendard-Black.woff',
+      weight: '900',
+    },
+    {
       path: '../../public/fonts/Pretendard-ExtraBold.woff2',
+      weight: '800',
+    },
+    {
+      path: '../../public/fonts/Pretendard-ExtraBold.woff',
       weight: '800',
     },
     {
@@ -15,7 +23,15 @@ export const pretendard = localFont({
       weight: '700',
     },
     {
+      path: '../../public/fonts/Pretendard-Bold.woff',
+      weight: '700',
+    },
+    {
       path: '../../public/fonts/Pretendard-SemiBold.woff2',
+      weight: '600',
+    },
+    {
+      path: '../../public/fonts/Pretendard-SemiBold.woff',
       weight: '600',
     },
     {
@@ -23,7 +39,15 @@ export const pretendard = localFont({
       weight: '500',
     },
     {
+      path: '../../public/fonts/Pretendard-Medium.woff',
+      weight: '500',
+    },
+    {
       path: '../../public/fonts/Pretendard-Regular.woff2',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Regular.woff',
       weight: '400',
     },
     {
@@ -31,11 +55,23 @@ export const pretendard = localFont({
       weight: '300',
     },
     {
+      path: '../../public/fonts/Pretendard-Light.woff',
+      weight: '300',
+    },
+    {
       path: '../../public/fonts/Pretendard-ExtraLight.woff2',
       weight: '200',
     },
     {
+      path: '../../public/fonts/Pretendard-ExtraLight.woff',
+      weight: '200',
+    },
+    {
       path: '../../public/fonts/Pretendard-Thin.woff2',
+      weight: '100',
+    },
+    {
+      path: '../../public/fonts/Pretendard-Thin.woff',
       weight: '100',
     },
   ],
